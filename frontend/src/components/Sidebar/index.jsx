51fd11c660f059141b0f410b07d0d29649cca4cd
tensoroneai <dev@tensorone.ai>
2024-11-18 15:40:18 -0800

    Light/dark mode UI overhaul (#2629)
    * Refactor workspace sidebar component styles (#2380)
    
    rely on css for conditional styles
    
    * New sidebar colors (#2381)
    
    new sidebar colors
    
    * Main container color update (#2382)
    
    * Dark mode setup themes (#2411)
    
    * setup generic tailwind theme + ability to add new themes
    
    * add theme context
    
    * use correct colors from design for sidebar + fix padding
    
    * Settings sidebar UI updates (#2416)
    
    settings sidebar ui updates
    
    * fix sidebar resizing/truncate issue on hover
    
    * Dark mode chat window (#2443)
    
    * Support XLSX files (#2403)
    
    * support xlsx files
    
    * lint
    
    * create seperate docs for each xlsx sheet
    
    * lint
    
    * use node-xlsx pkg for parsing xslx files
    
    * lint
    
    * update error handling
    
    ---------
    
    
    * wip chat window
    
    * ux+ux improvements and update new colors
    
    * chat window dark mode
    
    * remove comment
    
    ---------
    
    
    * Dark mode welcome page (#2444)
    
    * dark mode welcome page styles + refactor
    
    * remove AI_BACKGROUND_COLOR and USER_BACKGROUND_COLOR constants
    
    * Dark mode UI for admin and tools pages + mobile view improvements (#2454)
    
    * dark mode ui for admin and tools pages + mobile view improvements
    
    * lint
    
    * ai provider pages + options darkmode ui
    
    * placeholder generic class
    
    * appearance settings styles
    
    * ai providers mobile margin
    
    * dark mode styles for agent skills + experimental features
    
    * mobile styles on security settings
    
    * fine tune flow ui dark mode
    
    * workspace settings page
    
    * lint
    
    * Dark mode onboarding (#2461)
    
    dark mode onboarding
    
    * update all modals + normalize styles (#2471)
    
    * lint
    
    * Dark mode privacy & experimental pages (#2479)
    
    * document watch + privacy pages ui + mobile modal darkmode
    
    * lint
    
    * Dark mode login screens (#2483)
    
    * multi-user auth screen ui update
    
    * dark mode login screen + recovery key modals
    
    * remove unneeded import
    
    * Workspace preset commands modals dark mode (#2484)
    
    update workspace preset dark mode modal
    
    * Document pinning modal ui update (#2490)
    
    document pinning modal ui update
    
    * Experimental agreement modal dark mode (#2491)
    
    experimental agreement modal dark mode
    
    * Serp options dark mode (#2492)
    
    serp options dark mode
    
    * field fixes
    
    * attempt light mode wip
    
    * setting sidebar
    
    * Toasts and threads
    
    * main page content and privacy page
    
    * force rewrite for light
    
    * add border for light mode rightside content
    
    * more fixes
    
    * wip
    
    * wip
    
    * wip light mode implementation
    
    * wip dark light mode file picker
    
    * document picker light mode ui
    
    * slight ui tweaks
    
    * light mode fine tuning flow
    
    * light mode tweaks + qa fixes
    
    * fix md rendering of light mode + tooltip fixes
    
    * lint
    
    * qa bug fixes
    
    * Add developer hook for theme
    move provider to outmost layer
    
    * qa light mode bug fixes
    
    * Linting and hotfixes for UI
    
    * Light mode to dev
    
    * accept invite light mode ui fix
    
    * Fix onboarding inputs in dark mode
    
    * fix close icons
    last minute items
    
    * patch z-index on tooltips
    
    * patch light mode citations
    
    ---------

--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -4,13 +4,13 @@ import NewWorkspaceModal, {
   useNewWorkspaceModal,
 } from "../Modals/NewWorkspace";
 import ActiveWorkspaces from "./ActiveWorkspaces";
-import { USER_BACKGROUND_COLOR } from "@/utils/constants";
 import useLogo from "@/hooks/useLogo";
 import useUser from "@/hooks/useUser";
 import Footer from "../Footer";
 import SettingsButton from "../SettingsButton";
 import { Link } from "react-router-dom";
 import paths from "@/utils/paths";
+import { useTranslation } from "react-i18next";
 
 export default function Sidebar() {
   const { user } = useUser();
@@ -21,6 +21,7 @@ export default function Sidebar() {
     showModal: showNewWsModal,
     hideModal: hideNewWsModal,
   } = useNewWorkspaceModal();
+  const { t } = useTranslation();
 
   return (
     <div>
@@ -37,7 +38,7 @@ export default function Sidebar() {
       </Link>
       <div
         ref={sidebarRef}
-        className="relative m-[16px] rounded-[16px] bg-sidebar border-2 border-outline min-w-[250px] p-[10px] h-[calc(100%-76px)]"
+        className="relative m-[16px] rounded-[16px] bg-theme-bg-sidebar border-[2px] border-theme-sidebar-border light:border-none min-w-[250px] p-[10px] h-[calc(100%-76px)]"
       >
         <div className="flex flex-col h-full overflow-x-hidden">
           <div className="flex-grow flex flex-col min-w-[235px]">
@@ -47,11 +48,11 @@ export default function Sidebar() {
                   {(!user || user?.role !== "default") && (
                     <button
                       onClick={showNewWsModal}
-                      className="flex flex-grow w-[75%] h-[44px] gap-x-2 py-[5px] px-2.5 mb-2 bg-white rounded-[8px] text-sidebar justify-center items-center hover:bg-opacity-80 transition-all duration-300"
+                      className="light:bg-[#C2E7FE] light:hover:bg-[#C2E7FE]/50 flex flex-grow w-[75%] h-[44px] gap-x-2 py-[5px] px-2.5 mb-2 bg-white rounded-[8px] text-sidebar justify-center items-center hover:bg-opacity-80 transition-all duration-300"
                     >
                       <Plus size={18} weight="bold" />
                       <p className="text-sidebar text-sm font-semibold">
-                        New Workspace
+                        {t("new-workspace.title")}
                       </p>
                     </button>
                   )}
@@ -59,7 +60,7 @@ export default function Sidebar() {
                 <ActiveWorkspaces />
               </div>
             </div>
-            <div className="absolute bottom-0 left-0 right-0 pt-4 pb-3 rounded-b-[16px] bg-sidebar bg-opacity-80 backdrop-filter backdrop-blur-md z-10">
+            <div className="absolute bottom-0 left-0 right-0 pt-4 pb-3 rounded-b-[16px] bg-theme-bg-sidebar bg-opacity-80 backdrop-filter backdrop-blur-md z-10">
               <Footer />
             </div>
           </div>
@@ -81,6 +82,7 @@ export function SidebarMobileHeader() {
     hideModal: hideNewWsModal,
   } = useNewWorkspaceModal();
   const { user } = useUser();
+  const { t } = useTranslation();
 
   useEffect(() => {
     // Darkens the rest of the screen
@@ -101,11 +103,11 @@ export function SidebarMobileHeader() {
     <>
       <div
         aria-label="Show sidebar"
-        className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center px-4 py-2 bg-sidebar text-slate-200 shadow-lg h-16"
+        className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center px-4 py-2 bg-theme-bg-sidebar light:bg-white text-slate-200 shadow-lg h-16"
       >
         <button
           onClick={() => setShowSidebar(true)}
-          className="rounded-md p-2 flex items-center justify-center text-slate-200"
+          className="rounded-md p-2 flex items-center justify-center text-theme-text-secondary"
         >
           <List className="h-6 w-6" />
         </button>
@@ -130,12 +132,12 @@ export function SidebarMobileHeader() {
             showBgOverlay
               ? "transition-all opacity-1"
               : "transition-none opacity-0"
-          }  duration-500 fixed top-0 left-0 ${USER_BACKGROUND_COLOR} bg-opacity-75 w-screen h-screen`}
+          }  duration-500 fixed top-0 left-0 bg-theme-bg-secondary bg-opacity-75 w-screen h-screen`}
           onClick={() => setShowSidebar(false)}
         />
         <div
           ref={sidebarRef}
-          className="relative h-[100vh] fixed top-0 left-0  rounded-r-[26px] bg-sidebar w-[80%] p-[18px] "
+          className="relative h-[100vh] fixed top-0 left-0  rounded-r-[26px] bg-theme-bg-sidebar w-[80%] p-[18px] "
         >
           <div className="w-full h-full flex flex-col overflow-x-hidden items-between">
             {/* Header Information */}
@@ -167,7 +169,7 @@ export function SidebarMobileHeader() {
                       >
                         <Plus className="h-5 w-5" />
                         <p className="text-sidebar text-sm font-semibold">
-                          New Workspace
+                          {t("new-workspace.title")}
                         </p>
                       </button>
                     )}
@@ -175,7 +177,7 @@ export function SidebarMobileHeader() {
                   <ActiveWorkspaces />
                 </div>
               </div>
-              <div className="z-99 absolute bottom-0 left-0 right-0 pt-2 pb-6 rounded-br-[26px] bg-sidebar bg-opacity-80 backdrop-filter backdrop-blur-md">
+              <div className="z-99 absolute bottom-0 left-0 right-0 pt-2 pb-6 rounded-br-[26px] bg-theme-bg-sidebar bg-opacity-80 backdrop-filter backdrop-blur-md">
                 <Footer />
               </div>
             </div>
